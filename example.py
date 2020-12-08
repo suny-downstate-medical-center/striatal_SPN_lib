00fbae5e34bfe44008cccf46ac51a369ca45d9d8
barefoothiker <barefoothiker99@gmail.com>
2020-12-07 23:10:04 -0500

    latest

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ from common_functions import *
 # Load model mechanisms
 import neuron               as nrn
 import numpy as np
+import random
 nrn.load_mechanisms('mechanisms/single')
 h.load_file('stdlib.hoc')
 h.load_file('import3d.hoc')
@@ -60,8 +61,11 @@ for cell_index in model_iterator:
 
     # current needed to make the cell spike (if given to the soma)
     rheobase        =   model_sets[cell_index]['rheobase']
-
-    # record vectors
+    # Istim           =   h.IClamp(0.5, sec=cell.soma)
+    # Istim.delay     =   100
+    # Istim.dur       =   1000
+    # Istim.amp       =   (rheobase) *1e-3
+    # # record vectors
     tm  = h.Vector()
     tm.record(h._ref_t)
 
@@ -69,36 +73,49 @@ for cell_index in model_iterator:
     vm = h.Vector()
     vm.record(cell.soma(0.5)._ref_v)
 
+    # to get count of dendrites
+    count_dend = 0
+    for x in cell.dendlist:
+        count_dend += 1
+
+    rand_dend_nums = random.sample(range(1, len(range(count_dend))), 20)
+
+    print (rand_dend_nums)
+
+    clist = []
+
+    count = 0
+
     for c in cell.dendlist: # TODO test other dendritic locations!
+        print (" count = " + str(count))
+
+        count +=1
+
+        if count not in rand_dend_nums:
+
+            continue
 
         parentDendrites = getParentDendrites(c, [])
         distFromSoma = sum([x[1] for x in parentDendrites])
         print(parentDendrites)
 
+        count +=1
+
         # randomly add stim on a distal dendrite
         if len(parentDendrites) > 3 and distFromSoma > 100:
-            ''' DON'T...
-            print()
-            print ( " ******************** adding stim for dend " + str(c.name()))
-            Istim           =   h.IClamp(0.5, sec=c)
-            Istim.delay     =   100
-            Istim.dur       =   1000
-            Istim.amp       =   5 * (rheobase) *1e-3
-            addStimFlag = False
-            print(h.distance(0.5, sec=c), distFromSoma)
-            '''
 
             # create a single glut synapse (glutamate)
-            syn = h.glutamate( 0.5, sec=c)
-            # default ampa/nmda ratio = 1
-            # TODO test other ratios
-            #syn.ratio = 1
-            # TODO vary the time constants
-            # tau1_ampa, tau2_ampa, tau1_nmda, tau2_nmda
+            for loc in (np.arange(1,9,.2) )/10:
+                syn = h.glutamate( loc, sec=c)
+                # default ampa/nmda ratio = 1
+                # TODO test other ratios
+                syn.ratio = 1
+                # TODO vary the time constants
+                # tau1_ampa, tau2_ampa, tau1_nmda, tau2_nmda
 
             # create NetStim object
             # TODO test larger and smaller gbase
-            gbase          = 2.0e-3        # 0.3e-3
+            gbase          = 3.0e-3        # 0.3e-3
             ns             = h.NetStim()
             ns.start       = 100
             # TODO test other intervals!
@@ -122,8 +139,10 @@ for cell_index in model_iterator:
             nmda_current = h.Vector()
             nmda_current.record(syn._ref_i_nmda)
 
-            break   # set in first only...
-
+            clist.append(c)
+            # break   # set in first only...
+            # if count > 4:
+            #     break
     # run simulation
     h.finitialize(-85)
 
@@ -131,21 +150,26 @@ for cell_index in model_iterator:
     while h.t < 300:
         h.fadvance()
 
-    fig,ax = plt.subplots(2,1, figsize=(6,10), sharex='all')
-    ax[0].plot(tm.to_python(), vm.to_python(), label='soma')
-    ax[0].plot(tm.to_python(), vml.to_python(), label=c.name())
-    ax[1].plot(tm, ampa_current, label='ampa')
-    ax[1].plot(tm, nmda_current, label='nmda')
+    print (clist)
 
-    ax[0].set_title('Voltage in the soma and a stimulated dendrite')
-    ax[0].set_ylabel('Voltage (mV)')
-    ax[0].legend()
-    ax[1].set_title('synaptic current')
-    ax[1].set_ylabel('Current (nA)')
-    ax[1].set_xlabel('Time (ms)')
-    ax[1].legend()
+    for c in clist:
+        plt.clf()
+        print (" plotting for c = " + c.name())
+        fig,ax = plt.subplots(2,1, figsize=(6,10), sharex='all')
+        ax[0].plot(tm.to_python(), vm.to_python(), label='soma')
+        ax[0].plot(tm.to_python(), vml.to_python(), label=c.name())
+        ax[1].plot(tm, ampa_current, label='ampa')
+        ax[1].plot(tm, nmda_current, label='nmda')
 
-    plt.show()
+        ax[0].set_title('Voltage in the soma and a stimulated dendrite')
+        ax[0].set_ylabel('Voltage (mV)')
+        ax[0].legend()
+        ax[1].set_title('synaptic current')
+        ax[1].set_ylabel('Current (nA)')
+        ax[1].set_xlabel('Time (ms)')
+        ax[1].legend()
+
+        plt.savefig("output/final_out_" + c.name() + ".png")
 
     #plt.savefig("output/cell_"+ str(cell_index) + "_dendrite_" + str(c.name()) )
 
